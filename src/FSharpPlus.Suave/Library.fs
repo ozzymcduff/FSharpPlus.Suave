f73729d54cf07e693aa5e5747bcd424bc5ccbb40
Oskar Gewalli <oskar@gewalli.se>
2018-03-04 18:48:41 +0100

    How are things defined?

--- src/FSharpPlus.Suave/Library.fs
+++ src/FSharpPlus.Suave/Library.fs
@@ -1,7 +1,43 @@
 namespace FSharpPlus.Suave
 open FSharpPlus
 open Suave
+open Suave.WebPart
+open System
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+module internal WebPart=
+(* how it's defined in suave:
+  let bind (f: 'a -> Async<'b option>) (a: Async<'a option>) = async {
+    let! p = a
+    match p with
+    | None ->
+      return None
+    | Some q ->
+      let r = f q
+      return! r
+    }
+  let compose (first : 'a -> Async<'b option>) (second : 'b -> Async<'c option>)
+            : 'a -> Async<'c option> =
+    fun x ->
+      bind second (first x)
+  //'a -> Async<'a option>
+  *)
+  let map (f:'T->'U) (x:WebPart) = failwith "!"
+    //let inline map (f:'T->'U)=function
+    //|AccFailure e -> AccFailure e
+    //|AccSuccess a -> AccSuccess (f a)
+  let apply (f:'T->'U) (x:WebPart) :WebPart =
+    //WebPart.bind (fun x1 -> WebPart.bind (fun x2 -> async {return x1 x2}) x) f
+    failwith "!"
 
-module Say =
-    let hello name =
-        sprintf "Hello %s" name
+//'a -> Async<'a option>
+[<Extension;Sealed>]
+type WebPartExtensions=
+
+  // as Applicative
+  [<Extension>]static member Return x = WebPart.succeed x
+  // as Alternative (inherits from Applicative)
+  [<Extension>]static member inline get_Empty () = WebPart.never()
+  [<Extension>]static member (<*>) (f:'T->'U, x:WebPart) : WebPart = WebPart.apply f x
+  [<Extension>]static member inline Append (x:WebPart, y:WebPart) :WebPart =WebPart.compose x y
+  [<Extension>]static member inline Bind (x:WebPart, f:'T->WebPart) =WebPart.bind f x
--- tests/FSharpPlus.Suave.Tests/Tests.fs
+++ tests/FSharpPlus.Suave.Tests/Tests.fs
@@ -2,12 +2,34 @@ module Tests
 
 
 open Expecto
+open Suave
+open Suave.Filters
+//open Suave.Operators
+open Suave.RequestErrors
+open Suave.Successful
+open Suave.Writers
 open FSharpPlus.Suave
+open FSharpPlus
+let webPart ()=
+  let overview =
+    GET >=> fun (ctx) ->
+            async {
+              return! OK ("") ctx
+            }
+  let register =
+    POST >=> fun (ctx) ->
+            async {
+              return! OK ("") ctx
+            }
+
+  WebPart.choose [ path "/" >=> (OK "")
+                   path "/notes" >=> overview
+                   path "/note" >=> register]
 
 [<Tests>]
 let tests =
   testList "samples" [
     testCase "Say hello all" <| fun _ ->
-      let subject = Say.hello "all"
+      let subject = "all"
       Expect.equal subject "Hello all" "You didn't say hello"
   ]
