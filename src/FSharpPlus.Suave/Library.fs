61258f0785475becbd1f06d31d4023a414ca4012
Oskar Gewalli <oskar@gewalli.se>
2018-03-08 22:22:07 +0100

    What about this?

--- src/FSharpPlus.Suave/Library.fs
+++ src/FSharpPlus.Suave/Library.fs
@@ -11,29 +11,16 @@ open System.Net.Http
 // WebPart: 'a -> Async<'a option>
 // type WebPart = HttpContext -> Async<HttpContext option>
 // WebPartResult: Async<'a option>
-type WebPart<'a> = WebPart of ('a->Async<'a option>)
+//type WebPart<'a> = WebPart of ('a->Async<'a option>)
 type SuaveTask<'a> = SuaveTask of (Async<'a option>)
 module WebPart=
-  let unwrap (WebPart a) = a
-  let wrap a =WebPart a
-  let bind (f: 'x-> 'a -> Async<'b option>) (a: 'a-> Async<'a option>) :'a -> Async<'b option> = fun x-> async {
-    let! p = a x
-    match p with
-    | None ->
-      return None
-    | Some q ->
-      let r = f q
-      return! r x
-    }
-  let map (f: 'a -> 'b) (a: 'a-> Async<'a option>) :'a -> Async<'b option> = fun x-> async {
-    let! p = a x
-    match p with
-    | None ->
-      return None
-    | Some q ->
-      let r = f q
-      return Some r
-    }
+  //let unwrap (WebPart a) = a
+  let inline wrap (a:'a->Async<'a option>) = fun (ctx) ->
+      monad {
+        match ctx with
+        | Some c-> return! a c
+        | None -> return None
+      }
 module SuaveTask=
   module WP = Suave.WebPart
 
@@ -49,16 +36,16 @@ module SuaveTask=
       return Some r
     }
 
-
+(*
 module Successful=
   module S = Suave.Successful
-  let OK s= WebPart <| S.OK s
+  let OK s= WebPart.wrap <| S.OK s
 
 module Filters=
   module F = Suave.Filters
-  let GET=WebPart <| F.GET
-  let POST=WebPart <| F.POST
-
+  let GET=WebPart.wrap F.GET
+  let POST=WebPart.wrap F.POST
+*)
 type SuaveTask<'a> with
 
   [<Extension>]static member Return x= SuaveTask <| async { return Some x }
@@ -67,6 +54,7 @@ type SuaveTask<'a> with
 
   [<Extension>]static member inline Map (SuaveTask x, f:'a->'b) =SuaveTask <| SuaveTask.map f x
 
+(*
 type WebPart<'a> with
 
   [<Extension>]static member Return x= WebPart.wrap <| WebPart.succeed
@@ -74,3 +62,4 @@ type WebPart<'a> with
 
   [<Extension>]static member inline Bind (WebPart x, f) = WebPart <| WebPart.bind (f>>WebPart.unwrap) x
   [<Extension>]static member inline Map (WebPart x, f) = WebPart <| WebPart.map f x
+*)
